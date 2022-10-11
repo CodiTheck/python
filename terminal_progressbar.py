03f8b2460b522aa623fec2867c2600d80e5b3d53
Dr Mokira <arnoldmokira3d48@gamil.com>
2022-10-11 06:44:28 -0800

    terminal_progressbar.py is updated.

--- terminal_progressbar.py
+++ terminal_progressbar.py
@@ -1,15 +1,16 @@
 import math
+import time
 
 
-def progress_bar(progress, total):
+def progress_bar(progress, total, label=''):
     """ 
     Fonction de calcul et d'affichage de progression sur 
     le terminal.
     """
     percent = 100 * (progress / float(total));
-    bar     = 'O' * int(percent) + '-' * int(100 - float(percent));
-    print(f"\r|{bar}| {percent:.2f}%", end='\r');
-    if progress == total:
+    bar     = f'{chr(24)}' * int(percent) + '-' * int(100 - float(percent)); # chr(25) ==> code ASCII 26
+    print(f"\r|{bar}| {percent:.2f}% {label}", end='\r');
+    if progress >= total:
         # si la progression est complette, alors
         # on retourne a la ligne
         print();
@@ -17,6 +18,7 @@ def progress_bar(progress, total):
 
 
 if __name__ == '__main__':
+    """ Exemple d'utilisation de la fonction """
     numbers = [x * 5 for x in range(2000, 3000)];
     results = [];
 
@@ -24,5 +26,6 @@ if __name__ == '__main__':
     for i, x in enumerate(numbers):
         results.append(math.factorial(x));
         progress_bar(i + 1, len(numbers));
+        time.sleep(0.01);
 
 
